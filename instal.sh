5e4a048398c5b4fd77ee92acbf7f01fa85b5ef5b
AnimCreate12 <78071406+AnimCreate12@users.noreply.github.com>
2021-02-10 13:32:04 +0700

    SH
    Ketik sh instal.sh
    Untuk menjalankan

---
+++ instal.sh
@@ -0,0 +1,15 @@
+pkg install imagemagick -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install git -y
+pkg install unzip -y
+pkg install zip -y
+git clone https://github.com/AnimCreate12/AnimS
+cd AnimS
+ls
+unzip AnimS
+cd
+ls
+cd ANIMBOTV3
+bash install.sh
+echo "[*]SUKSES BRO KETIK node index.js :)"
